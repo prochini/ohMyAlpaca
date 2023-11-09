c5f522266d50e2412e8d5bc53f7aa89c5a6a7ea7
prochini <a77471@alumni.fcu.edu.tw>
2023-11-09 19:07:49 +1000

    add firebase & primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  value = 3;
+  userData!: Observable<any>;
+  constructor(private firestore: Firestore) {
+    this.getData();
+  }
+
+
+
+
+
+
+  async addData(f: any) {
+    console.log(f.value);
+    const userProfileCollection = collection(this.firestore, 'users');
+    console.log(userProfileCollection);
+
+    addDoc(userProfileCollection, f.value).then(() => {
+      console.log('Data saved successfully');
+
+    }).catch((err) => {
+      console.log(err);
+
+    })
+  }
+
+  getData() {
+    const userProfileCollection = collection(this.firestore, 'users');
+
+    collectionData(userProfileCollection).subscribe(val => {
+      console.log(val);
+
+    })
+
+    this.userData = collectionData(userProfileCollection);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+import { SliderModule } from 'primeng/slider';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    provideFirebaseApp(() => initializeApp({ "projectId": "ohmyalpaca-e5554", "appId": "1:58194086457:web:4a4ff9138a11b727435be8", "storageBucket": "ohmyalpaca-e5554.appspot.com", "apiKey": "AIzaSyDzp-XI95JKhpu22Bq5GjaOeGJ1swgPnPc", "authDomain": "ohmyalpaca-e5554.firebaseapp.com", "messagingSenderId": "58194086457", "measurementId": "G-RCKM1Y4WL7" })),
+    provideFirestore(() => getFirestore()),
+    SliderModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
