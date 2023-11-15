55dba481bc3d744f9045089b5c1202afbeb457d2
Ken <ken60173@gmail.com>
2023-11-15 20:52:29 +1000

    alpaca everdaysignin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,33 @@ export class AppComponent {
   title = 'my-ALPACA';
   value = 3;
   userData!: Observable<any>;
+  PlayerData!: Observable<any>;
+  PlayerModel!: {
+    Account: string,
+    Diamond: Number,
+    Email: string,
+    Gender: Number,
+    Money: Number,
+    Password: string,
+    id: string,
+  }[]
   constructor(private firestore: Firestore) {
     this.getData();
+    this.getPlayer();
   }
 
   addData(f: any) {
     console.log(f.value);
     const userProfileCollection = collection(this.firestore, 'users');
+    addDoc(userProfileCollection, f.value).then(() => {
+      console.log('Data saved successfully');
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+  addPlayer(f: any) {
+    console.log(f.value);
+    const userProfileCollection = collection(this.firestore, 'player');
     addDoc(userProfileCollection, f.value).then(() => {
       console.log('Data saved successfully');
     }).catch((err) => {
@@ -34,6 +54,15 @@ export class AppComponent {
     this.userData = collectionData(userProfileCollection, { idField: 'id' });
   }
 
+  getPlayer() {
+    const userProfileCollection = collection(this.firestore, 'player');
+    collectionData(userProfileCollection, { idField: 'id' }).subscribe(val => {
+      console.log(val);
+    })
+    this.PlayerData = collectionData(userProfileCollection, { idField: 'id' });
+
+  }
+
   updateData(id: string) {
     const docInstence = doc(this.firestore, 'users', id);
     const updateData = {
@@ -47,5 +76,19 @@ export class AppComponent {
 
     })
   }
+  updateSignIn(id: string, money: number) {
+    const docInstence = doc(this.firestore, 'player', id);
+    const updateData = {
+      Money: money + 1000
+
+    }
+    updateDoc(docInstence, updateData).then(() => {
+      console.log("dataupdate");
+    }).catch((err) => {
+      console.log(err);
+
+    })
+  }
+
 
 }
